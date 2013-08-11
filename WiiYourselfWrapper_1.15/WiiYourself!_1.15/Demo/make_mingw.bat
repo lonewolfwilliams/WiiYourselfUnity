c9a91c5650bc4eb59a6ea6ce22f0c337b8971936
Gareth Williams <Gareth@lonewolfwilliams.com>
2013-08-11 22:57:13 +0100

    rebuild whole visual studio solution should solve various building issues

--- WiiYourselfWrapper_1.15/WiiYourself!_1.15/Demo/make_mingw.bat
+++
@@ -1,4 +0,0 @@
-g++ -Wall -g0 -O2 -c Demo.cpp -o Demo.o
-g++ -o Demo.exe Demo.o -s -L. -lwiimote -lwinmm -lhid -lsetupapi
-
-@rem g++ -o Demo.exe Demo.o wiimote.dll -lwinmm ; to create Demo.exe that uses wiimote.dll
